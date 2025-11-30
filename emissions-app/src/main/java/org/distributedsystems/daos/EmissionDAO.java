286fd689cddf5e5d7e6737d0938636328e17acf0
Hamzah Naveid <128648654+hamzahnaveid@users.noreply.github.com>
2025-11-30 00:10:15 +0000

    XML file parsing to DB

--- emissions-app/src/main/java/org/distributedsystems/daos/EmissionDAO.java
+++ emissions-app/src/main/java/org/distributedsystems/daos/EmissionDAO.java
@@ -1,61 +1,50 @@
 package org.distributedsystems.daos;
 
-import java.util.ArrayList;
+import java.util.ArrayList; 
 import java.util.List;
 
 import org.distributedsystems.entities.Emission;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import jakarta.persistence.Persistence;
+import jakarta.transaction.Transactional;
 
+@ApplicationScoped
 public class EmissionDAO {
 	
-	protected static EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaPU");
+	@Inject
+	EntityManager em;
 	
+	
+	@Transactional
 	public void persist(Emission emission) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		em.persist(emission);
-		em.getTransaction().commit();
-		em.close();
 	}
 	
+	@Transactional
 	public void remove(Emission emission) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		em.remove(em.merge(emission));
-		em.getTransaction().commit();
-		em.close();
 	}
 	
+	@Transactional
 	public Emission merge(Emission emission) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		Emission updatedEmission = em.merge(emission);
-		em.getTransaction().commit();
-		em.close();
 		return updatedEmission;
 	}
 	
+	@Transactional
 	public List<Emission> getAllEmissions() {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		List<Emission> emissionsFromDb = new ArrayList<Emission>();
 		emissionsFromDb = em.createNamedQuery("Emission.findAll").getResultList();
-		em.getTransaction().commit();
-		em.close();
 		return emissionsFromDb;
 	}
 	
+	@Transactional
 	public List<Emission> findEmissionsByCategory(String category) {
-		EntityManager em = emf.createEntityManager();
-		em.getTransaction().begin();
 		List<Emission> emissionsWithCategory = new ArrayList<Emission>();
 		emissionsWithCategory = em.createNamedQuery("Emission.findByCategory").setParameter("category", category).getResultList();
-		em.getTransaction().commit();
-		em.close();
-		return emissionsWithCategory;	
+		return emissionsWithCategory;
 	}
 
 }
--- emissions-app/src/main/java/org/distributedsystems/resources/EmissionsAppResource.java
+++ emissions-app/src/main/java/org/distributedsystems/resources/EmissionsAppResource.java
@@ -1,16 +1,64 @@
 package org.distributedsystems.resources;
 
+import java.io.IOException; 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.distributedsystems.daos.EmissionDAO;
+import org.distributedsystems.entities.Emission;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/app")
+@ApplicationScoped
 public class EmissionsAppResource {
-
+	
+	@Inject
+	EmissionDAO emissionDao;
+    
     @GET
+    @Path("/parseXml")
     @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello World";
+    public String parseXml() throws ParserConfigurationException, SAXException, IOException {
+    	String result = "";
+    	DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+    	DocumentBuilder builder = factory.newDocumentBuilder();
+    	Document document = builder.parse("/Users/hamzahnaveid/Documents/MMR_IRArticle23T1_IE_2016v2.xml");
+
+    	
+    	NodeList rows = document.getElementsByTagName("Row");
+    	
+    	for (int i = 0; i <rows.getLength(); i++) {
+    		NodeList children = rows.item(i).getChildNodes();
+    		
+    		for (int j = 0; j < children.getLength(); j++) {
+    			Node node = children.item(j);
+    			
+    			if (node.getNodeName().contains("Year") && node.getTextContent().equalsIgnoreCase("2023") && children.item(j+2).getTextContent().equalsIgnoreCase("WEM") && !children.item(j+8).getTextContent().isEmpty() && Double.parseDouble(children.item(j+8).getTextContent()) > 0) {
+    				String category = children.item(j-2).getTextContent();
+    				int year = Integer.parseInt(node.getTextContent());
+    				String scenario = children.item(j+2).getTextContent();
+    				String gasUnits = children.item(j+4).getTextContent();
+    				double value = Double.parseDouble(children.item(j+8).getTextContent());
+    				
+    				Emission emission = new Emission(category, null, year, scenario, gasUnits, value);
+    				emissionDao.persist(emission);
+    			}
+    			
+    		}
+    	}
+    	return result;
     }
+    
 }
